13dbe26b88e28f604c41c2bcc6dcf4eeb2ef63a1
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-09-01 09:00:11 +0200

    chromium profile dirs

--- bin/start-teams.sh
+++ bin/start-teams.sh
@@ -4,16 +4,16 @@
 hyprctl dispatch workspace 7
 
 # Launch all Teams instances with delays
-uwsm app -- chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory='Profile 1' --ozone-platform=wayland --app='https://teams.microsoft.com' &
+uwsm app -- chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory="Profile 1" --ozone-platform=wayland --app='https://teams.microsoft.com' &
 PID1=$!
 sleep 0.5
-uwsm app -- chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory='Profile 2' --ozone-platform=wayland --app='https://teams.microsoft.com' &
+uwsm app -- chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory="Profile 2" --ozone-platform=wayland --app='https://teams.microsoft.com' &
 PID2=$!
 sleep 0.5
-uwsm app -- chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory='Profile 3' --ozone-platform=wayland --app='https://teams.microsoft.com' &
+uwsm app -- chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory="Profile 3" --ozone-platform=wayland --app='https://teams.microsoft.com' &
 PID3=$!
 sleep 0.5
-uwsm app -- chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory='Profile 4' --ozone-platform=wayland --app='https://teams.microsoft.com'
+uwsm app -- chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory="Profile 4" --ozone-platform=wayland --app='https://teams.microsoft.com'
 PID4=$!
 
 # Wait for all windows to fully load
