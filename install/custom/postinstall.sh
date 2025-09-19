7e5d03bc7b1922c653aaeb3e6c7bc86c451064fc
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-09-19 23:37:46 +0200

    install yay packages post

--- install/custom/postinstall.sh
+++ install/custom/postinstall.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/custom/custom-yay.packages" | grep -v '^$')
+yay -S --noconfirm --needed "${packages[@]}"
+
 yay -S --needed --noconfirm \
   calcure \
   ttf-ms-win11-auto
