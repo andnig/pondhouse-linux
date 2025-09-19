92cee2d1e15903eac23d26c9d0f103a17c4000c5
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-09-19 19:20:50 +0200

    use pacman to installed local packages

--- install/custom/yay.sh
+++ install/custom/yay.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# The packages are downloaded during iso creation and built there
+# Therefore we can use pacman here without yay being installed
 mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/custom/custom-yay.packages" | grep -v '^$')
-yay -S --needed --noconfirm "${packages[@]}"
+sudo pacman -S --noconfirm --needed "${packages[@]}"
