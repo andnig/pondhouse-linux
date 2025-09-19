a8c81aa68095a5bfbf9e66e4961d6a310fb031da
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-09-19 20:09:24 +0200

    use better pacman install

--- install/custom/pacman.sh
+++ install/custom/pacman.sh
@@ -1,46 +1,7 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm --needed \
-  base-devel \
-  ca-certificates \
-  cmake \
-  containerd \
-  devtools \
-  doxx \
-  fontconfig \
-  gcc \
-  gettext \
-  gnupg \
-  grim \
-  hplip \
-  httpie \
-  kitty \
-  krew \
-  kubectl \
-  libappindicator-gtk3 \
-  lua \
-  luajit \
-  meson \
-  npm \
-  pandoc \
-  python \
-  python-pip \
-  python-pynvim \
-  python-ruff \
-  python-setuptools \
-  seahorse \
-  stow \
-  swappy \
-  syncthing \
-  tailscale \
-  thunderbird \
-  tmux \
-  uv \
-  wget \
-  yazi \
-  yq \
-  7zip \
-  ntfs-3g
+mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/custom/custom-pacman.packages" | grep -v '^$')
+sudo pacman -S --noconfirm --needed "${packages[@]}"
 
 echo "Installing tpm (tmux plugin manager)"
 rm -rf ~/.tmux/plugins/tpm
