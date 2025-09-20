3b94164875fa9d2230138668796acb7934339872
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-09-20 19:28:30 +0200

    some logs

--- install/custom/postinstall.sh
+++ install/custom/postinstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+gum style --foreground 212 --bold "Installing all AUR packages, if missing..."
 mapfile -t packages < <(grep -v '^#' "$HOME/.local/share/omarchy/install/custom/custom-yay.packages" | grep -v '^$')
 yay -Sy --noconfirm --needed "${packages[@]}"
 
+gum style --foreground 212 --bold "Installing Microsoft fonts..."
 yay -S --needed --noconfirm \
   ttf-ms-win11-auto
 
