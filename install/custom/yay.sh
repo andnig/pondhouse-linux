61a32721750dc99547a4154e602ae3f41da66d7d
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-09-20 13:38:11 +0200

    yay

--- install/custom/yay.sh
+++ install/custom/yay.sh
@@ -3,4 +3,9 @@
 # The packages are downloaded during iso creation and built there
 # Therefore we can use pacman here without yay being installed
 mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/custom/custom-yay.packages" | grep -v '^$')
-sudo pacman -S --noconfirm --needed "${packages[@]}"
+
+for pkg in "${packages[@]}"; do
+  if ! sudo pacman -S --noconfirm --needed "$pkg"; then
+    echo "Failed to install $pkg, skipping..."
+  fi
+done
