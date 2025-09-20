01316e23877c37713dde9cab9931ff1ab7427cdf
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-09-20 16:12:50 +0200

    postinstall

--- install/custom/postinstall.sh
+++ install/custom/postinstall.sh
@@ -10,20 +10,25 @@ gum style --foreground 212 --bold "Installing Tailscale..."
 omarchy-install-tailscale
 
 # Ask which code editors to install
-gum style --foreground 212 --bold "Select code editors to install (Space to select, Enter to confirm):"
+gum style --foreground 212 --bold "Select optional packages to install (Space to select, Enter to confirm):"
 selected=$(gum choose --no-limit --selected.foreground="212" \
   "VSCode" \
-  "Cursor")
+  "Cursor" \
+  "Windows 11")
 
 # Install selected editors
 if echo "$selected" | grep -q "VSCode"; then
-  omarchy-launch-floating-terminal-with-presentation echo 'Installing VSCode...' && \
-  sudo pacman -S --noconfirm visual-studio-code-bin && \
-  setsid gtk-launch code
+  omarchy-launch-floating-terminal-with-presentation echo 'Installing VSCode...' &&
+    sudo pacman -S --noconfirm visual-studio-code-bin &&
+    setsid gtk-launch code
 fi
 
 if echo "$selected" | grep -q "Cursor"; then
-  omarchy-launch-floating-terminal-with-presentation echo 'Installing Cursor...' && \
-  sudo pacman -S --noconfirm cursor-bin && \
-  setsid gtk-launch cursor
+  omarchy-launch-floating-terminal-with-presentation echo 'Installing Cursor...' &&
+    sudo pacman -S --noconfirm cursor-bin &&
+    setsid gtk-launch cursor
+fi
+
+if echo "$selected" | grep -q "Windows 11"; then
+  omarchy-launch-tiling-terminal-with-presentation omarchy-install-virt-windows
 fi
