bda99fa51978b6098dc513a3c488c6f007985136
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-09-20 17:19:49 +0200

    postinstall

--- install/custom/postinstall.sh
+++ install/custom/postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mapfile -t packages < <(grep -v '^#' "$HOME/.local/share/omarchy/install/custom/custom-yay.packages" | grep -v '^$')
-yay -S --noconfirm --needed "${packages[@]}"
+yay -Sy --noconfirm --needed "${packages[@]}"
 
 yay -S --needed --noconfirm \
   ttf-ms-win11-auto
