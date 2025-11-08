bcd9fb517c3dee24d68c11d00575acb19f0b610f
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-11-08 15:10:43 +0100

    use default text editor for mimetypes

--- migrations/1761758552.sh
+++ migrations/1761758552.sh
@@ -1,21 +1,35 @@
-echo "Set nvim as default via xdg-mime"
+echo "Set text editor as default for all text mime types"
 
 rm ~/.local/share/applications/nvim.desktop || true
 
-xdg-mime default nvim.desktop text/plain
-xdg-mime default nvim.desktop text/english
-xdg-mime default nvim.desktop text/x-makefile
-xdg-mime default nvim.desktop text/x-c++hdr
-xdg-mime default nvim.desktop text/x-c++src
-xdg-mime default nvim.desktop text/x-chdr
-xdg-mime default nvim.desktop text/x-csrc
-xdg-mime default nvim.desktop text/x-java
-xdg-mime default nvim.desktop text/x-moc
-xdg-mime default nvim.desktop text/x-pascal
-xdg-mime default nvim.desktop text/x-tcl
-xdg-mime default nvim.desktop text/x-tex
-xdg-mime default nvim.desktop application/x-shellscript
-xdg-mime default nvim.desktop text/x-c
-xdg-mime default nvim.desktop text/x-c++
-xdg-mime default nvim.desktop application/xml
-xdg-mime default nvim.desktop text/xml
+# Query current default for text/plain
+current_text_editor=$(xdg-mime query default text/plain 2>/dev/null || echo "")
+
+# Use current default if set, otherwise fall back to nvim
+if [ -n "$current_text_editor" ]; then
+  echo "Using current default text editor: $current_text_editor"
+  text_editor="$current_text_editor"
+else
+  echo "No default text editor set, using nvim as default"
+  text_editor="nvim.desktop"
+fi
+
+# Set the editor for all text-related mime types
+xdg-mime default "$text_editor" text/plain
+xdg-mime default "$text_editor" text/english
+xdg-mime default "$text_editor" text/x-makefile
+xdg-mime default "$text_editor" text/x-c++hdr
+xdg-mime default "$text_editor" text/x-c++src
+xdg-mime default "$text_editor" text/x-chdr
+xdg-mime default "$text_editor" text/x-csrc
+xdg-mime default "$text_editor" text/x-java
+xdg-mime default "$text_editor" text/x-moc
+xdg-mime default "$text_editor" text/x-pascal
+xdg-mime default "$text_editor" text/x-tcl
+xdg-mime default "$text_editor" text/x-tex
+xdg-mime default "$text_editor" application/x-shellscript
+xdg-mime default "$text_editor" text/x-c
+xdg-mime default "$text_editor" text/x-c++
+xdg-mime default "$text_editor" application/xml
+xdg-mime default "$text_editor" text/xml
+
