6b26dc1974e0f943d896c87c8db164d28ad08202
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-11-08 15:11:31 +0100

    nvim defaults

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.util.dot" },
         { import = "lazyvim.plugins.extras.editor.snacks_picker" },
         { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
+        { import = "lazyvim.plugins.extras.coding.yanky" },
         -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
         -- import/override with your plugins
         { import = "plugins" },
--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -7,6 +7,14 @@ return {
         },
         picker = {
             sources = {
+                gh_issue = {
+                    -- your gh_issue picker configuration comes here
+                    -- or leave it empty to use the default settings
+                },
+                gh_pr = {
+                    -- your gh_pr picker configuration comes here
+                    -- or leave it empty to use the default settings
+                },
                 explorer = {
                     hidden = true,
                     ignored = true,
