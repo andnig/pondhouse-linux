40830764b5a65b82ada85bf5b5d95bac572570c2
Andreas Nigg <andreas.nigg@devopsandmore.com>
2026-08-05 10:49:05 +0200

    fix herdr migration

--- migrations/1784569952.sh
+++ migrations/1784569952.sh
@@ -1,10 +1,11 @@
 echo "Install Herdr and herdr-reviewr, then link Herdr config"
 
 omarchy-pkg-aur-add herdr-bin
-bash "$OMARCHY_PATH/install/custom/herdr-reviewr.sh"
 
 if [[ -e $HOME/.config/herdr || -L $HOME/.config/herdr ]]; then
   echo "Herdr config already exists, skipping"
 else
   omarchy-refresh-config herdr/config.toml
 fi
+
+bash "$OMARCHY_PATH/install/custom/herdr-reviewr.sh"
